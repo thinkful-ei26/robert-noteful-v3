355b3dce68291fa6602d647fdd093c0267cbbf95
RP Boyle <rpboyle11@yahoo.com>
2018-11-28 16:04:19 -0500

    all of my test scripts are up and running

--- server.js
+++ server.js
@@ -43,15 +43,15 @@ app.use((err, req, res, next) => {
   }
 });
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error('\n === Did you remember to start `mongod`? === \n');
-    console.error(err);
-  });
+if (require.main === module) {
+  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
 
-// Listen for incoming connections
-if (process.env.NODE_ENV !== 'test') {
+  // Listen for incoming connections
   app.listen(PORT, function () {
     console.info(`Server listening on ${this.address().port}`);
   }).on('error', err => {
