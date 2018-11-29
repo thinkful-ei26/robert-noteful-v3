029add796f86cc9af24876bb425217e08f5e944c
RP Boyle <rpboyle11@yahoo.com>
2018-11-29 15:03:42 -0500

    added folder.js

--- models/note.js
+++ models/note.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  content: String
+  content: String,
+  folderID: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }
 });
 
 // Add `createdAt` and `updatedAt` fields
