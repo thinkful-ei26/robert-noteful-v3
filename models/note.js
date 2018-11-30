46f6e650916e7d9359a36c5f9155c4d59b77826f
RP Boyle <rpboyle11@yahoo.com>
2018-11-30 11:06:20 -0500

    updating note.js

--- models/note.js
+++ models/note.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: String,
-  folderID: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder' }
+  folderID: { type: mongoose.Schema.Types.ObjectId, ref: 'Folder', required: true }
 });
 
 // Add `createdAt` and `updatedAt` fields
--- scratch/queries.js
+++ scratch/queries.js
@@ -116,3 +116,33 @@ const Folder = require('../models/folder');
 //     console.error(`ERROR: ${err.message}`);
 //     console.error(err);
 //   });
+
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchTerm = 'lady gaga';
+    let filter = {};
+
+    if (searchTerm) {
+      filter.title = { $regex: searchTerm, $options: 'i' }, 
+      filter.content = { $regex: searchTerm, $options: 'i'};
+    }
+
+    return Note.find({$or: [
+      { title: filter.title },
+      { content: filter.content}
+    ]}).sort({ updatedAt: 'desc' });
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
+
+
+
